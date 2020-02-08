41efa11836e1676f995621f45ceb8b431d62ba63
Dylan Coffey <dcoffey20@gmail.com>
2020-02-08 11:00:26 -0600

    create file paths

---
+++ migrations/20200208165830-create-members.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Members', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      user_name: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      languages: {
+        type: Sequelize.STRING
+      },
+      gender_orientation: {
+        type: Sequelize.INTEGER
+      },
+      about_me: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Members');
+  }
+};
---
+++ models/members.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Members = sequelize.define('Members', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    user_name: DataTypes.STRING,
+    gender: DataTypes.INTEGER,
+    email: DataTypes.STRING,
+    languages: DataTypes.STRING,
+    gender_orientation: DataTypes.INTEGER,
+    about_me: DataTypes.STRING
+  }, {});
+  Members.associate = function(models) {
+    // associations can be defined here
+  };
+  return Members;
+};
