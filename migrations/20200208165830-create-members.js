78b02f91a093b17702e1d24293a5ad48caaaa1f6
Kogan <koganp42@gmail.com>
2020-02-11 19:27:07 -0600

    Created models and migration files, associations tbd

--- migrations/20200208165830-create-members.js
+++ migrations/20200208165830-create-members.js
@@ -30,7 +30,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       about_me: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
--- migrations/20200208170755-create-messages.js
+++ migrations/20200208170755-create-messages.js
@@ -15,7 +15,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       message: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
