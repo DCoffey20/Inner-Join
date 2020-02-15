41bc48e8094ad1a31dd50f9d3aecd094a83760e5
DCOFFEY20 <54090952+DCoffey20@users.noreply.github.com>
2020-02-15 14:51:44 -0600

    updated signup page (#35)
    Co-authored-by: Kogan Pack <koganp42@gmail.com>

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,22 +1,20 @@
 const path = require("path");
+const db = require("../models");
 
 // var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
 
-  app.get("/", function(req, res) {
-    // If the user already has an account send them to the members page
-    // if (req.user) {
-    //   res.redirect("/members");
-    // }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
-
-  app.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
-    // if (req.user) {
-    //   res.redirect("/members");
-    // }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+  app.post("/api/signup", function(req, res) {
+    db.Members.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(function() {
+        res.redirect(307, "/api/login");
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
   });
 }
