38f0ec11da0e0f382d2f4c6d75078beffb03b68f
Kogan Pack <koganp42@gmail.com>
2020-02-15 13:35:56 -0600

    First half of user matches working, get user working (#34)

--- controllers/members-controller.js
+++ controllers/members-controller.js
@@ -1,27 +1,31 @@
 const db = require("../models");
 const passport = require("../config/passport");
+const express = require("express");
 
-module.exports = function(app) {
+const router = express.Router();
 
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    res.json(req.user);
+
+
+router.post("/api/login", passport.authenticate("local"), function (req, res) {
+  res.json(req.user);
+});
+
+router.get("/members/:id", function (req, res) {
+  db.Members.findOne({
+    raw: true,
+    where: {
+      id: req.params.id
+    }
+  }).then(function (res) {
+    // let hbsMember = { member: data };
+    // res.render("memprof", hbsMember);
+    console.log(res);
   });
+});
 
-  app.get("/api/members/:id", function (req, res) {
-    db.Members.findOne({
-      raw : true,
-      where: {
-        id: req.params.id
-      }
-    }).then(function (data) {
-      let hbsMember = { member: data };
-      res.render("memprof", hbsMember);
-    });
-  });
+router.post("/api/members", function (req, res) {
 
-  app.post("/api/members", function (req, res) {
-    
-    db.Burger.create({
+  db.Members.create({
     first_name: req.body.firstName,
     last_name: req.body.lastName,
     user_name: req.body.userName,
@@ -35,7 +39,7 @@ module.exports = function(app) {
   });
 });
 
-  app.put("/api/members/:id", function (req, res) {
+router.put("/api/members/:id", function (req, res) {
   console.log("put request" + req.params.id);
   console.log(`${JSON.stringify(req.body)} from put request`)
   db.Members.update({
@@ -47,35 +51,19 @@ module.exports = function(app) {
     gender_orientation: req.body.genderPref,
     about_me: DataTypes.TEXT,
     password: req.body.password
-  },{
-    where: {
-      id: req.params.id
-    }
-  }).then(result => {
-    console.log(`from put ${result}`);
-    if (result.changedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
+  }, {
+      where: {
+        id: req.params.id
+      }
+    }).then(result => {
+      console.log(`from put ${result}`);
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
 });
 
-//  The below is where I'm working on doing the requests to left join members on languages for find one and left join all members on languages.
-
-app.get("/api/members", function (req, res) {
-  db.Members.findAll({
-    where: {
-      id: req.params.id
-    },
-    include: [db.Author]
-  }).then(function(dbPost) {
-    res.json(dbPost);
-
-  }).then(function (data) {
-    let hbsMember = { member: data };
-    res.render("memprof", hbsMember);
-  });
-});
-}
+module.exports = router;
