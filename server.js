ccbf77cb6c5c18d9da66a1afde96f5d512d44d11
Dylan Coffey <dcoffey20@gmail.com>
2020-02-15 11:24:45 -0600

    created login routes for server.js

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const passport = require("./config/passport");
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 // Creating express app and configuring middleware needed for authentication
 const app = express();
@@ -23,8 +21,11 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Requiring our routes
-// require("./routes/html-routes.js")(app);
+require("./controllers/login-controller")(app);
 // require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
