76a77c71a9c2d121345a4771fef8a97a997afcdc
Kogan Pack <koganp42@gmail.com>
2020-02-15 09:53:44 -0600

    Password seed update (#23)
    * members controller in progress
    
    * Added a password for seed users.

--- seed.sql
+++ seed.sql
@@ -1,14 +1,14 @@
-insert into members (first_name, last_name, user_name, gender, email, gender_orientation, createdAt, updatedAt)
-values("Pete", "Johnson", "Pete_Johnson", 1, "PJ@email.com", 2, now(), now()),
-("Darcy", "Emerson", "Darcy_Emerson", 2, "DE@email.com", 1, now(), now()),
-("Nala", "Wade", "Nala_Wade", 2, "NW@emial.com", 1, now(), now()),
-("Carmen", "Southern", "Carmen_Southern", 2, "CS@email.com", 1, now(), now()),
-("Rowan", "Horner", "Rowan_Horner", 1, "RH@email.com", 2, now(), now()),
-("Jonah", "Dowling", "Jonah_Dowling", 1, "JD@email.com", 2, now(), now()),
-("Colton", "Noel", "Colton_Noel", 1, "CN@email.com", 2, now(), now()),
-("Griff", "Guy", "Griff_Guy", 1, "GG@email.com", 2, now(), now()),
-("Tess", "Carroll", "Tess_Carroll", 2, "TC@email.com", 1, now(), now()),
-("Elana", "Connor", "Elana_Connor", 2, "EC@email.com", 1, now(), now()); 
+insert into members (first_name, last_name, user_name, gender, email, gender_orientation, password, createdAt, updatedAt)
+values("Pete", "Johnson", "Pete_Johnson", 1, "PJ@email.com", 2, "Password1", now(), now()),
+("Darcy", "Emerson", "Darcy_Emerson", 2, "DE@email.com", 1, "Password2", now(), now()),
+("Nala", "Wade", "Nala_Wade", 2, "NW@emial.com", 1, "Password3", now(), now()),
+("Carmen", "Southern", "Carmen_Southern", 2, "CS@email.com", 1, "Password4", now(), now()),
+("Rowan", "Horner", "Rowan_Horner", 1, "RH@email.com", 2, "Password5", now(), now()),
+("Jonah", "Dowling", "Jonah_Dowling", 1, "JD@email.com", 2, "Password6", now(), now()),
+("Colton", "Noel", "Colton_Noel", 1, "CN@email.com", 2, "Password7", now(), now()),
+("Griff", "Guy", "Griff_Guy", 1, "GG@email.com", 2, "Password8", now(), now()),
+("Tess", "Carroll", "Tess_Carroll", 2, "TC@email.com", 1, "Password9", now(), now()),
+("Elana", "Connor", "Elana_Connor", 2, "EC@email.com", 1, "Password10", now(), now()); 
 
 insert into languages (javascript, c, csharp, java, ruby, PHP, swift, CPlusPlus, R, perl, assembly, objectivec, createdAt, updatedAt)
 values(true, false, true, true, false, false, false, false, true, false, false, false, now(), now()),
